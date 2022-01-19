c37d8171aa26f71e3db79cb1482174237aeae819
Eric L <ewl+git@lavar.de>
2022-01-19 07:26:56 +0100

    Transform Python script into Ansible modules

---
+++ plugins/module_utils/domainctl.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python3
+
+#
+# Simple DNS client for Bawue.Net
+#
+# Copyright 2020 Andreas Thienemann <andreas@bawue.net>
+#
+# This code is licensed for use and distribution under the GPLv3+
+#
+
+import argparse
+import requests
+import socket
+import sys
+import time
+from dns.resolver import NXDOMAIN, Resolver
+from bs4 import BeautifulSoup
+from requests.auth import HTTPBasicAuth
+
+base_url = "https://my.bawue.net/domains.php"
+auth_ns = "ns1.bawue.net"
+username = None
+password = None
+
+type_table = {
+    'A': 1,
+    'CNAME': 2,
+    'MX': 4,
+    'TXT': 8,
+    'AAAA': 128,
+    'NS': 256,
+    'SRV': 512,
+}
+
+
+def error(msg):
+    sys.stderr.write("ERROR:   " + msg + "\n")
+
+
+def warn(msg):
+    sys.stderr.write("WARNING: " + msg + "\n")
+
+
+def parse_html_table(table, formdata=False):
+    """ Parse a html table, return headers and data """
+    headers = [x.text for x in table.find('tr').find_all('th')]
+    data = []
+    metadata = []
+    for row in table.find_all('tr')[1:]:
+        if formdata and row.find('form'):
+            metadata.append(dict(
+                [(input.get('name'), input.get('value'))
+                    for input in row.find('form').find_all('input')]))
+        else:
+            metadata.append({})
+        data.append([x.text for x in row.find_all('td') if len(x.text) > 0])
+    if formdata:
+        return ([x for x in headers if len(x) > 0], data, metadata)
+    else:
+        return ([x for x in headers if len(x) > 0], data)
+
+
+def get_domain_data(username, password):
+    """ Get user owned domains """
+    r = requests.get('%s' % base_url, auth=HTTPBasicAuth(username, password))
+    if r.status_code != 200:
+        raise RuntimeError("Not authorized")
+    soup = BeautifulSoup(r.text, 'html.parser')
+    data = soup.find('table')
+    headers, data = parse_html_table(data)
+    return (headers[:-1], [x[:-1] for x in data])
+
+
+def get_domains(username, password):
+    """ Get a list of domains """
+    return sorted([x[0] for x in get_domain_data(username, password)[1]])
+
+
+def print_domains(output, username, password):
+    """ Pretty print a table of domains owned """
+    headers, data = get_domain_data(username, password)
+    print(output(data, headers=headers))
+
+
+def get_domain_records(domain):
+    """ Get RRs from a domain """
+    params = {'domain': domain, 'action': 'edit'}
+    r = requests.get('%s' % base_url, auth=HTTPBasicAuth(username, password),
+                     params=params)
+    if r.status_code != 200:
+        raise RuntimeError("Not authorized")
+    soup = BeautifulSoup(r.text, 'html.parser')
+    data = soup.find('table')
+    headers, data, metadata = parse_html_table(data, True)
+    table = []
+    for idx in range(len(data)):
+        table.append(data[idx] + [metadata[idx]])
+    return (headers + ['metadata'], table)
+
+
+def print_domain_records(domain, output):
+    """ Pretty print a table of domain contents """
+    headers, data = get_domain_records(domain)
+    print(output([x[:-1] for x in data], headers=headers[:-1]))
+
+
+def add_record(domain, host, dnstype, rr):
+    """ Add a record to the DNS """
+    fullhost = host + '.' + domain
+    headers, records = get_domain_records(domain)
+    # keep only the records fitting our criteria
+    records = [y for y in [dict(zip(headers, x)) for x in records]
+               if y['Owner'] == fullhost and y['Ressource Record'] == rr]
+    if records:
+        warn(f"Record {fullhost} with ressource {rr} already exists")
+        return None  # nothing to be done
+    params = {
+        'owner': host, 'type': type_table[dnstype], 'RRecord': rr,
+        'Domainname': domain, 'action': 'domain-dns-admin-commit-zone-entry'
+    }
+    r = requests.get('%s' % base_url, auth=HTTPBasicAuth(username, password),
+                     params=params)
+    if r.status_code != 200:
+        error(f"Not authorized to add {host} to {domain} due to {r}")
+        return False
+    else:
+        return True
+
+
+def remove_record(domain, host, dnstype, rr):
+    """ Remove a record from the DNS """
+    fullhost = host + '.' + domain
+    headers, records = get_domain_records(domain)
+    # keep only the records fitting our criteria
+    records = [y for y in [dict(zip(headers, x)) for x in records]
+               if y['Owner'] == fullhost and y['Ressource Record'] == rr]
+    if not records:
+        warn(f"Record {fullhost} with ressource {rr} not found")
+        return None  # nothing to be done
+    for r in records:
+        rr_id = r['metadata']['zoneentryid']
+        params = {
+            'Domainname': domain, 'zoneentryid': rr_id,
+            'action': 'domain-dns-admin-del-zone-entry'
+        }
+        r = requests.get('%s' % base_url,
+                         auth=HTTPBasicAuth(username, password),
+                         params=params)
+        if r.status_code != 200:
+            error(
+                f"Not authorized to remove {host} from {domain} due to {r}")
+            return False
+        # we don't leave the loop so that all records with the same
+        # combination are removed
+    return True
+
+
+def query_dns_server(record, type):
+    resolver = Resolver(configure=False)
+    resolver.nameservers = [socket.gethostbyname(auth_ns)]
+    resolver.timeout = 5
+    resolver.lifetime = 5
+    try:
+        dns_query = resolver.resolve(record, type)
+        return dns_query
+    except NXDOMAIN:
+        return False
+
+
+def wait_for_record_add(domain, host, type, rr):
+    print("Waiting for DNS change...",)
+    for i in range(22):
+        answer = query_dns_server("%s.%s." % (host, domain), type)
+        try:
+            for i in answer.response.answer:
+                for j in i.items:
+                    if rr in j.to_text():
+                        return
+        except (AttributeError, TypeError):
+            pass
+        time.sleep(30)
+        print(".",)
+    raise RuntimeError("Timeout exceeded waiting for DNS change...")
+
+
+def wait_for_record_remove(domain, host, type, rr):
+    print("Waiting for DNS change...",)
+    for i in range(22):
+        if not query_dns_server("%s.%s." % (host, domain), type):
+            return
+        time.sleep(30)
+        print(".",)
+    raise RuntimeError("Timeout exceeded waiting for DNS change...")
+
+
+def output_table(data, headers):
+    import tabulate
+    return tabulate.tabulate(data, headers=headers)
+
+
+def output_json(data, headers):
+    import json
+    return json.dumps([dict(zip(headers, x)) for x in data], indent=2)
+
+
+def output_yaml(data, headers):
+    import yaml
+    return yaml.dump([dict(zip(headers, x)) for x in data], indent=2)
+
+
+def main():
+    description = """Bawue.Net DNS client
+
+Erlaubt via dem MyBawue.Net Webinterface einfach einen DNS Eintrag
+hinzuzufügen oder zu entfernen"""
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("action", type=str, help="one possible DNS action",
+                        choices=['list_domains', 'list_records',
+                                 'add_record', 'remove_record'])
+    parser.add_argument("--username", type=str, help="username",
+                        required=True)
+    parser.add_argument("--password", type=str, help="password",
+                        required=True)
+    parser.add_argument("--host", type=str, help="host name (without domain)")
+    parser.add_argument("--domain", type=str, help="domain")
+    parser.add_argument("--type", type=str, help="type")
+    parser.add_argument("--rr", type=str, help="rr")
+    parser.add_argument("--wait", action="store_true", help="wait")
+    parser.add_argument("--format", type=str, help="output format",
+                        choices=['table', 'yaml', 'json'], default='table')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # choose the right output format function
+    output = globals()['output_' + args.format]
+
+    # execute the selected action
+    if args.action == 'list_domains':
+        print_domains(output, args.username, args.password)
+    elif args.action == 'list_records':
+        if not args.domain:
+            error("--domain muss definiert sein")
+            sys.exit(1)
+        if args.domain not in get_domains():
+            error("%s gehört dem Nutzer nicht" % args.domain)
+            sys.exit(2)
+        print_domain_records(args.domain, output)
+    elif args.action == 'add_record' or args.action == 'remove_record':
+        for var in ('domain', 'host', 'type', 'rr'):
+            if not getattr(args, var):
+                error("--%s muss definiert sein" % var)
+                sys.exit(1)
+        if args.domain not in get_domains():
+            error("%s gehört dem Nutzer nicht" % args.domain)
+            sys.exit(2)
+        # call the action function by its name
+        ret = globals()[args.action](
+            args.domain, args.host, args.type, args.rr)
+        if ret:  # action was successful
+            if args.wait:
+                wait_for_record_add(args.domain, args.host, args.type, args.rr)
+        elif ret is None:
+            sys.exit(-1)  # nothing modified
+        else:  # error
+            sys.exit(3)
+
+
+if __name__ == "__main__":
+    main()
---
+++ plugins/modules/bwnet_domains_info.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2020, Your Name <YourName@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from ..module_utils import domainctl
+
+DOCUMENTATION = r'''
+---
+module: bwnet_domains_info
+
+short_description: get a list of domains owned by given user
+
+version_added: "1.0.0"
+
+description: returns a list of DNS domains owned by given user
+
+options:
+    username:
+        description: name of the user
+        required: true
+        type: str
+    password:
+        description: password of the user
+        required: true
+        type: str
+    data:
+        description: gather more data about domains
+        required: false
+        type: bool
+        default: false
+author:
+    - Eric Lavarde (@ericzolf)
+'''
+
+EXAMPLES = r'''
+# get list of domains
+- name: list John Doe's owned domains
+  bawunet.domainctl.bwnet_domains_info:
+    username: johndoe
+    password: secret
+  register: __domains_list
+'''
+
+RETURN = r'''
+domains:
+    description: list of DNS domains owned by given user
+    type: list
+    returned: always
+    sample:
+        - example.de
+        - example.eu
+domains_data:
+    description: data about the listed domains
+    type: list
+    returned: optional
+    sample: [
+            {
+                "Dienstname": ".de Domainhosting",
+                "Domain-Typ": "Automatischer Standard",
+                "Domainname": "example.de",
+                "Mailserver": "Spamfilter",
+                "Webserver": "virtweb02.bawue.net"
+            },
+            {
+                "Dienstname": ".eu Domainhosting",
+                "Domain-Typ": "Automatischer Standard",
+                "Domainname": "example.eu",
+                "Mailserver": "Spamfilter",
+                "Webserver": "virtweb02.bawue.net"
+            }
+        ]
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        username=dict(type='str', required=True),
+        password=dict(type='str', required=True, no_log=True),
+        data=dict(type='bool', required=False),
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        domains=[],
+        domains_data=[],
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    result['domains'] = domainctl.get_domains(
+            module.params['username'], module.params['password'])
+    if module.params['data']:
+        headers, data = domainctl.get_domain_data(
+            module.params['username'], module.params['password'])
+        result['domains_data'] = [dict(zip(headers, x)) for x in data]
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
