a3aeef601667ceb6931f242153e65b1c6aba72d7
Andreas Thienemann <andreas@bawue.net>
2023-03-05 12:42:02 +0100

    Rebase against master and merge Eric's more robust add/remove branch
    Closes: https://github.com/bawuenet/domainctl/pull/4

--- domainctl.py
+++ domainctl.py
@@ -33,6 +33,14 @@ type_table = {
 }
 
 
+def error(msg):
+    sys.stderr.write("ERROR:   " + msg + "\n")
+
+
+def warn(msg):
+    sys.stderr.write("WARNING: " + msg + "\n")
+
+
 def parse_html_table(table, formdata=False):
     """Parse a html table, return headers and data"""
     headers = [x.text for x in table.find("tr").find_all("th")]
@@ -102,11 +110,22 @@ def print_domain_records(domain, output):
     print(output([x[:-1] for x in data], headers=headers[:-1]))
 
 
-def add_record(domain, host, type, rr):
+def add_record(domain, host, dnstype, rr):
     """Add a record to the DNS"""
+    fullhost = host + "." + domain
+    headers, records = get_domain_records(domain)
+    # keep only the records fitting our criteria
+    records = [
+        y
+        for y in [dict(zip(headers, x)) for x in records]
+        if y["Owner"] == fullhost and y["Ressource Record"] == rr
+    ]
+    if records:
+        warn(f"Record {fullhost} with ressource {rr} already exists")
+        return None  # nothing to be done
     params = {
         "owner": host,
-        "type": type_table[type],
+        "type": type_table[dnstype],
         "RRecord": rr,
         "Domainname": domain,
         "action": "domain-dns-admin-commit-zone-entry",
@@ -115,26 +134,41 @@ def add_record(domain, host, type, rr):
         "%s" % base_url, auth=HTTPBasicAuth(username, password), params=params
     )
     if r.status_code != 200:
-        raise RuntimeError("Not authorized")
+        error(f"Not authorized to add {host} to {domain} due to {r}")
+        return False
+    else:
+        return True
 
 
-def remove_record(domain, host, type, rr):
+def remove_record(domain, host, dnstype, rr):
     """Remove a record from the DNS"""
+    fullhost = host + "." + domain
     headers, records = get_domain_records(domain)
+    # keep only the records fitting our criteria
+    records = [
+        y
+        for y in [dict(zip(headers, x)) for x in records]
+        if y["Owner"] == fullhost and y["Ressource Record"] == rr
+    ]
+    if not records:
+        warn(f"Record {fullhost} with ressource {rr} not found")
+        return None  # nothing to be done
     for r in records:
-        r = dict(zip(headers, r))
-        if "%s.%s" % (host, domain) == r["Owner"] and rr == r["Ressource Record"]:
-            rr_id = r["metadata"]["zoneentryid"]
-            params = {
-                "Domainname": domain,
-                "zoneentryid": rr_id,
-                "action": "domain-dns-admin-del-zone-entry",
-            }
-            r = requests.get(
-                "%s" % base_url, auth=HTTPBasicAuth(username, password), params=params
-            )
-            if r.status_code != 200:
-                raise RuntimeError("Not authorized")
+        rr_id = r["metadata"]["zoneentryid"]
+        params = {
+            "Domainname": domain,
+            "zoneentryid": rr_id,
+            "action": "domain-dns-admin-del-zone-entry",
+        }
+        r = requests.get(
+            "%s" % base_url, auth=HTTPBasicAuth(username, password), params=params
+        )
+        if r.status_code != 200:
+            error(f"Not authorized to remove {host} from {domain} due to {r}")
+            return False
+        # we don't leave the loop so that all records with the same
+        # combination are removed
+    return True
 
 
 def query_dns_server(record, type):
@@ -143,7 +177,7 @@ def query_dns_server(record, type):
     resolver.timeout = 5
     resolver.lifetime = 5
     try:
-        dns_query = resolver.query(record, type)
+        dns_query = resolver.resolve(record, type)
         return dns_query
     except NXDOMAIN:
         return False
@@ -216,7 +250,7 @@ hinzuzufügen oder zu entfernen"""
     )
     parser.add_argument("--username", type=str, help="username", required=True)
     parser.add_argument("--password", type=str, help="password", required=True)
-    parser.add_argument("--host", type=str, help="host")
+    parser.add_argument("--host", type=str, help="host name (without domain)")
     parser.add_argument("--domain", type=str, help="domain")
     parser.add_argument("--type", type=str, help="type")
     parser.add_argument("--rr", type=str, help="rr")
@@ -245,34 +279,29 @@ hinzuzufügen oder zu entfernen"""
         print_domains(output)
     elif args.action == "list_records":
         if not args.domain:
-            print("--domain muss definiert sein")
+            error("--domain muss definiert sein")
             sys.exit(1)
         if args.domain not in get_domains():
-            print("%s gehört dem Nutzer nicht" % args.domain)
+            error("%s gehört dem Nutzer nicht" % args.domain)
             sys.exit(2)
         print_domain_records(args.domain, output)
-    elif args.action == "add_record":
+    elif args.action == "add_record" or args.action == "remove_record":
         for var in ("domain", "host", "type", "rr"):
             if not getattr(args, var):
-                print("--%s muss definiert sein" % var)
+                error("--%s muss definiert sein" % var)
                 sys.exit(1)
         if args.domain not in get_domains():
-            print("%s gehört dem Nutzer nicht" % args.domain)
+            error("%s gehört dem Nutzer nicht" % args.domain)
             sys.exit(2)
-        add_record(args.domain, args.host, args.type, args.rr)
-        if args.wait:
-            wait_for_record_add(args.domain, args.host, args.type, args.rr)
-    elif args.action == "remove_record":
-        for var in ("domain", "host", "type", "rr"):
-            if not getattr(args, var):
-                print("--%s muss definiert sein" % var)
-                sys.exit(1)
-        if args.domain not in get_domains():
-            print("%s gehört dem Nutzer nicht" % args.domain)
-            sys.exit(2)
-        remove_record(args.domain, args.host, args.type, args.rr)
-        if args.wait:
-            wait_for_record_remove(args.domain, args.host, args.type, args.rr)
+        # call the action function by its name
+        ret = globals()[args.action](args.domain, args.host, args.type, args.rr)
+        if ret:  # action was successful
+            if args.wait:
+                wait_for_record_add(args.domain, args.host, args.type, args.rr)
+        elif ret is None:
+            sys.exit(-1)  # nothing modified
+        else:  # error
+            sys.exit(3)
 
 
 if __name__ == "__main__":
