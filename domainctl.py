584777eb9958cdb44108ff3ca549ee6602e54b6f
Andreas Thienemann <andreas@bawue.net>
2023-03-07 18:02:22 +0100

    Turn domainctl into a library and a user script

--- domainctl.py
+++ domainctl.py
@@ -3,212 +3,32 @@
 #
 # Simple DNS client for Bawue.Net
 #
-# Copyright 2020 Andreas Thienemann <andreas@bawue.net>
+# Copyright 2020-2023 Andreas Thienemann <andreas@bawue.net>
 #
 # This code is licensed for use and distribution under the GPLv3+
 #
 
 import argparse
-import requests
-import socket
+from bawuenet.domains import DomainsAPI
 import sys
-import time
-from dns.resolver import NXDOMAIN, Resolver, LifetimeTimeout
-from bs4 import BeautifulSoup
-from requests.auth import HTTPBasicAuth
-
-base_url = "https://my.bawue.net/domains.php"
-auth_ns = "ns1.bawue.net"
-username = None
-password = None
-
-type_table = {
-    "A": 1,
-    "CNAME": 2,
-    "MX": 4,
-    "TXT": 8,
-    "AAAA": 128,
-    "NS": 256,
-    "SRV": 512,
-}
-
 
 def error(msg):
     sys.stderr.write("ERROR:   " + msg + "\n")
-
 
 def warn(msg):
     sys.stderr.write("WARNING: " + msg + "\n")
 
 
-def parse_html_table(table, formdata=False):
-    """Parse a html table, return headers and data"""
-    headers = [x.text for x in table.find("tr").find_all("th")]
-    data = []
-    metadata = []
-    for row in table.find_all("tr")[1:]:
-        if formdata and row.find("form"):
-            metadata.append(
-                dict(
-                    [
-                        (input.get("name"), input.get("value"))
-                        for input in row.find("form").find_all("input")
-                    ]
-                )
-            )
-        else:
-            metadata.append({})
-        data.append([x.text.strip() for x in row.find_all("td") if len(x.text) > 0])
-    if formdata:
-        return ([x for x in headers if len(x) > 0], data, metadata)
-    else:
-        return ([x for x in headers if len(x) > 0], data)
-
-
-def get_domain_data(username, password):
-    """Get user owned domains"""
-    r = requests.get("%s" % base_url, auth=HTTPBasicAuth(username, password))
-    if not r.ok:
-        raise RuntimeError(f"Request failed due to {r.reason} ({r.status_code})")
-    soup = BeautifulSoup(r.text, "html.parser")
-    data = soup.find("table")
-    headers, data = parse_html_table(data)
-    return (headers[:-1], [x[:-1] for x in data])
-
-
-def get_domains(username, password):
-    """Get a list of domains"""
-    return sorted([x[0] for x in get_domain_data(username, password)[1]])
-
-
-def print_domains(output, username, password):
+def print_domains(output, client):
     """Pretty print a table of domains owned"""
-    headers, data = get_domain_data(username, password)
+    headers, data = client.get_domain_data()
     print(output(data, headers=headers))
 
 
-def get_domain_records(domain, username, password):
-    """Get RRs from a domain"""
-    params = {"domain": domain, "action": "edit"}
-    r = requests.get(
-        "%s" % base_url, auth=HTTPBasicAuth(username, password), params=params
-    )
-    if not r.ok:
-        raise RuntimeError(f"Request failed due to {r.reason} ({r.status_code})")
-    soup = BeautifulSoup(r.text, "html.parser")
-    data = soup.find("table")
-    headers, data, metadata = parse_html_table(data, True)
-    table = []
-    for idx in range(len(data)):
-        table.append(data[idx] + [metadata[idx]])
-    return (headers + ["metadata"], table)
-
-
-def print_domain_records(domain, output, username, password):
+def print_domain_records(domain, output, client):
     """Pretty print a table of domain contents"""
-    headers, data = get_domain_records(domain, username, password)
+    headers, data = client.get_domain_records(domain)
     print(output([x[:-1] for x in data], headers=headers[:-1]))
-
-
-def add_record(domain, host, dnstype, rr, username, password):
-    """Add a record to the DNS"""
-    fullhost = host + "." + domain
-    headers, records = get_domain_records(domain, username, password)
-    # keep only the records fitting our criteria
-    records = [
-        y
-        for y in [dict(zip(headers, x)) for x in records]
-        if y["Owner"] == fullhost and y["Ressource Record"] == rr
-    ]
-    if records:
-        warn(f"Record {fullhost} with ressource {rr} already exists")
-        return None  # nothing to be done
-    params = {
-        "owner": host,
-        "type": type_table[dnstype],
-        "RRecord": rr,
-        "Domainname": domain,
-        "action": "domain-dns-admin-commit-zone-entry",
-    }
-    r = requests.get(
-        "%s" % base_url, auth=HTTPBasicAuth(username, password), params=params
-    )
-    if r.status_code != 200:
-        raise RuntimeError(f"Request failed due to {r.reason} ({r.status_code})")
-    else:
-        return True
-
-
-def remove_record(domain, host, dnstype, rr, username, password):
-    """Remove a record from the DNS"""
-    fullhost = host + "." + domain
-    headers, records = get_domain_records(domain, username, password)
-    # keep only the records fitting our criteria
-    records = [
-        y
-        for y in [dict(zip(headers, x)) for x in records]
-        if y["Owner"] == fullhost and y["Ressource Record"] == rr
-    ]
-    if not records:
-        warn(f"Record {fullhost} with ressource {rr} not found")
-        return None  # nothing to be done
-    for r in records:
-        rr_id = r["metadata"]["zoneentryid"]
-        params = {
-            "Domainname": domain,
-            "zoneentryid": rr_id,
-            "action": "domain-dns-admin-del-zone-entry",
-        }
-        r = requests.get(
-            "%s" % base_url, auth=HTTPBasicAuth(username, password), params=params
-        )
-        if r.status_code != 200:
-            raise RuntimeError(f"Request failed due to {r.reason} ({r.status_code})")
-        # we don't leave the loop so that all records with the same
-        # combination are removed
-    return True
-
-
-def query_dns_server(record, type):
-    resolver = Resolver(configure=False)
-    resolver.nameservers = [socket.gethostbyname(auth_ns)]
-    resolver.timeout = 5
-    resolver.lifetime = 5
-    try:
-        dns_query = resolver.resolve(record, type)
-        return dns_query
-    except (NXDOMAIN, LifetimeTimeout):
-        return False
-
-
-def wait_for_add_record(domain, host, type, rr):
-    for i in range(22):
-        answer = query_dns_server("%s.%s." % (host, domain), type)
-        try:
-            for i in answer.response.answer:
-                for j in i.items:
-                    if rr in j.to_text():
-                        sys.stdout.write("\n")
-                        sys.stdout.flush()
-                        return
-        except (AttributeError, TypeError):
-            pass
-        time.sleep(30)
-        sys.stdout.write(".")
-        sys.stdout.flush()
-    raise RuntimeError("Timeout exceeded waiting for DNS change...")
-
-
-def wait_for_remove_record(domain, host, type, rr):
-    for i in range(22):
-        if not query_dns_server("%s.%s." % (host, domain), type):
-            sys.stdout.write("\n")
-            sys.stdout.flush()
-            return
-        time.sleep(30)
-        sys.stdout.write(".")
-        sys.stdout.flush()
-    raise RuntimeError("Timeout exceeded waiting for DNS change...")
 
 
 def output_table(data, headers):
@@ -262,28 +82,30 @@ hinzuzufügen oder zu entfernen"""
     # choose the right output format function
     output = globals()["output_" + args.format]
 
+    domain_api = DomainsAPI(args.username, args.password)
+
     # execute the selected action
     if args.action == "list_domains":
-        print_domains(output, args.username, args.password)
+        print_domains(output, domain_api)
     elif args.action == "list_records":
         if not args.domain:
             error("--domain muss definiert sein")
             sys.exit(1)
-        if args.domain not in get_domains(args.username, args.password):
+        if args.domain not in domain_api.get_domains():
             error("%s gehört dem Nutzer nicht" % args.domain)
             sys.exit(2)
-        print_domain_records(args.domain, output, args.username, args.password)
+        print_domain_records(args.domain, output, domain_api)
     elif args.action == "add_record" or args.action == "remove_record":
         for var in ("domain", "host", "type", "rr"):
             if not getattr(args, var):
                 error("--%s muss definiert sein" % var)
                 sys.exit(1)
-        if args.domain not in get_domains(args.username, args.password):
+        if args.domain not in domain_api.get_domains():
             error("%s gehört dem Nutzer nicht" % args.domain)
             sys.exit(2)
         # call the action function by its name
-        ret = globals()[args.action](
-            args.domain, args.host, args.type, args.rr, args.username, args.password
+        ret = getattr(domain_api, args.action)(
+            args.domain, args.host, args.type, args.rr
         )
         if ret is None:  # nothing modified
             sys.exit(-1)
@@ -291,7 +113,7 @@ hinzuzufügen oder zu entfernen"""
             print(
                 "Waiting for DNS change...",
             )
-            globals()["wait_for_" + args.action](
+            getattr(domain_api, "wait_for_" + args.action)(
                 args.domain, args.host, args.type, args.rr
             )
 
